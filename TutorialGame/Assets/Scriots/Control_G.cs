5cfc273fd457a3c3c781b6998207cb4efb058c4f
ivodesouzabarbosa <ivosouzabarbosa@gmail.com>
2021-06-06 22:42:35 -0300

    prefab

---
+++ TutorialGame/Assets/Scriots/Control_G.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control_G : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<Control_level> control_levelList = new List<Control_level>();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TutorialGame/Assets/Scriots/Control_level.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control_level : MonoBehaviour
+{
+    public List<Control_level> platlevelList = new List<Control_level>();// lista que ira receber as plataformas instanciadas do level 1
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
